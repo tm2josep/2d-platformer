7d0ab3269abbede3a64bd8453c0cff9c425dafef
Mammen Joseph <tm2josep@edu.uwaterloo.ca>
2019-09-04 15:02:09 +1000

    enable animations on sprites

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,19 @@
 // imports
-import { loadImage, loadLevel, loadAnimation } from './loaders';
-import { makeFixedLayer, drawTilesFromJson } from './canvasUtils';
+import { loadLevel, loadAnimation } from './loaders';
 
-import TileSet from './TileSet';
 import Sprite from './Sprite';
-import Compositor from './Compositor';
 import Entity from './Entity';
 import Timer from './Timer';
 import Keyboard from './KeyboardState';
 import Velocity from './Traits/Velocity';
-import { TILESIZE, WIDTH, HEIGHT, GRAVITY } from './constants';
+
+import { GRAVITY } from './constants';
 
 // Document Setup
 const canvas = <HTMLCanvasElement>document.getElementById('screen');
 const context = canvas.getContext('2d');
 
 // Logic
-const comp = new Compositor();
 const keyboard = new Keyboard();
 const SPACE = 32;
 
@@ -35,14 +32,14 @@ Promise.all([
     })
 
     player.addTrait(new Velocity());
-    player.trait('velocity').vec.set(200, -600);
+    player.trait('velocity').vec.set(0, 0);
 
     level.comp.addLayers((context: CanvasRenderingContext2D) => player.draw(context));
 
     const timer = new Timer(1 / 60, (delta: number) => {
         player.update(delta);
         let playerVel: Velocity = player.trait('velocity');
-        playerVel.vec.y += GRAVITY * delta;
+        // playerVel.vec.y += GRAVITY * delta;
         level.comp.draw(context);
     });
 
